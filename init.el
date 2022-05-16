2f819ca3c28fb91c873bcddd9dd4830c05bb5e4d
Eason0210 <aqua0210@163.com>
2022-05-17 01:43:49 +0800

    Setup window

--- init.el
+++ init.el
@@ -99,6 +99,66 @@
      (consult-file-externally (dired-get-filename))
      arg)))
 
+;;; Working with Windows within frames
+
+(use-package window
+  :bind (([f7] . sanityinc/split-window)
+         ("C-c <down>". sanityinc/toggle-current-window-dedication)
+         :map ctl-x-4-map
+         ("s" . toggle-window-split))
+  :config
+  (bind-key "C-x 2" (split-window-func-with-other-buffer 'split-window-vertically))
+  (bind-key "C-x 3" (split-window-func-with-other-buffer 'split-window-horizontally))
+  :preface
+  ;; When splitting window, show (other-buffer) in the new window
+  (defun split-window-func-with-other-buffer (split-function)
+    "Use SPLIT-FUNCTION to split window."
+    (lambda (&optional arg)
+      "Split this window and switch to the new window unless ARG is provided."
+      (interactive "P")
+      (funcall split-function)
+      (let ((target-window (next-window)))
+        (set-window-buffer target-window (other-buffer))
+        (unless arg
+          (select-window target-window)))))
+
+  (defun toggle-window-split ()
+    "Toggle window split from vertical to horizontal."
+    (interactive)
+    (if (> (length (window-list)) 2)
+        (error "Can't toggle with more than 2 windows")
+      (let ((was-full-height (window-full-height-p)))
+        (delete-other-windows)
+        (if was-full-height
+            (split-window-vertically)
+          (split-window-horizontally))
+        (save-selected-window
+          (other-window 1)
+          (switch-to-buffer (other-buffer))))))
+
+  ;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
+  (defun sanityinc/split-window()
+    "Split the window to see the most recent buffer in the other window.
+Call a second time to restore the original window configuration."
+    (interactive)
+    (if (eq last-command 'sanityinc/split-window)
+        (progn
+          (jump-to-register :sanityinc/split-window)
+          (setq this-command 'sanityinc/unsplit-window))
+      (window-configuration-to-register :sanityinc/split-window)
+      (switch-to-buffer-other-window nil)))
+
+  ;; Toggle to dedicated window
+  (defun sanityinc/toggle-current-window-dedication ()
+    "Toggle whether the current window is dedicated to its current buffer."
+    (interactive)
+    (let* ((window (selected-window))
+           (was-dedicated (window-dedicated-p window)))
+      (set-window-dedicated-p window (not was-dedicated))
+      (message "Window %sdedicated to %s"
+               (if was-dedicated "no longer " "")
+               (buffer-name)))))
+
 ;;; Settings for tracking recent files
 
 (use-package recentf
