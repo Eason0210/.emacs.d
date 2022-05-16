8698db15be118bbaaeb31b454d0b7a2daefc2601
Eason0210 <aqua0210@163.com>
2022-05-17 01:12:09 +0800

    Assimilate diff-hl 1.8.8-79-ge84209e

--- init.el
+++ init.el
@@ -81,6 +81,15 @@
 
 ;;; Long tail
 
+;;; Version control
+
+(use-package diff-hl
+  :bind (:map diff-hl-mode-map
+              ("<left-fringe> <mouse-1>" . diff-hl-diff-goto-hunk))
+  :hook ((magit-post-refresh . diff-hl-magit-post-refresh)
+         (after-init . global-diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode)))
+
 ;;; Configure default locale
 
 (progn ; `charset'
