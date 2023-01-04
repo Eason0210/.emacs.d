e01f900f71360888b8f2b9995e7de5dec7d3a2d5
Eason0210 <aqua0210@163.com>
2023-01-04 12:16:43 +0800

    package: Don't use MELPA

--- early-init.el
+++ early-init.el
@@ -6,11 +6,6 @@
 
 (setq package-enable-at-startup nil)
 
-(with-eval-after-load 'package
-  (add-to-list 'package-archives
-               (cons "melpa" "https://melpa.org/packages/")
-               t))
-
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
