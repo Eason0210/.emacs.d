866c8c980ff30dce058d77e7f45382c88c80d8f6
Eason0210 <aqua0210@163.com>
2023-07-31 07:46:20 +0800

    simple: Bind <f8> to scratch-buffer

--- init.el
+++ init.el
@@ -446,6 +446,7 @@ Call a second time to restore the original window configuration."
   ("C-x x p" . pop-to-mark-command)
   ("C-x C-." . pop-global-mark)
   ([remap capitalize-word] . capitalize-dwim)
+  ("<f8>" . scratch-buffer)
   :custom
   (indent-tabs-mode nil)
   (save-interprogram-paste-before-kill t)
