390fc30c066ce1034626c12c39ea4ec12c1435ba
Eason0210 <aqua0210@163.com>
2023-08-14 12:39:53 +0800

    color-theme-sanityinc-tomorrow: Prefer the light color theme

--- init.el
+++ init.el
@@ -76,7 +76,27 @@
 (use-package color-theme-sanityinc-tomorrow
   :custom
   (custom-safe-themes t)
-  (custom-enabled-themes '(sanityinc-tomorrow-bright)))
+  (custom-enabled-themes '(sanityinc-tomorrow-day))
+  :hook (after-init . reapply-themes)
+  :config
+  (defun reapply-themes ()
+    "Forcibly load the themes listed in `custom-enabled-themes'."
+    (dolist (theme custom-enabled-themes)
+      (unless (custom-theme-p theme)
+        (load-theme theme)))
+    (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+  (defun light ()
+    "Activate a light color theme."
+    (interactive)
+    (setq custom-enabled-themes '(sanityinc-tomorrow-day))
+    (reapply-themes))
+
+  (defun dark ()
+    "Activate a dark color theme."
+    (interactive)
+    (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
+    (reapply-themes)))
 
 (use-package ns-auto-titlebar
   :when *is-a-mac*
