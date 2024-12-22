82c3a057073aca201db024a13b52300fd2c8a028
Eason0210 <aqua0210@163.com>
2024-12-22 08:54:10 +0800

    Assimilate gcmh 0089f9c

--- init.el
+++ init.el
@@ -71,6 +71,13 @@
 
 ;;; Long tail
 
+;; General performance tuning
+(use-package gcmh
+  :diminish
+  :custom
+  (gcmh-high-cons-threshold (* 128 1024 1024))
+  :hook (after-init . gcmh-mode))
+
 ;;; Theme
 
 (use-package color-theme-sanityinc-tomorrow
